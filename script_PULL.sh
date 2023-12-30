45e20cc71df224098932e434002cd1b607f475d5
ntumanov <grooov173@yandex.ru>
2023-12-31 02:06:43 +0400

    Обновил скрипт загрузки

--- script_PULL.sh
+++ script_PULL.sh
@@ -1,2 +1,8 @@
 git fetch
-git reset --hard HEAD
+git checkout -- Baldur.db
+git checkout -- Baldur.db.old
+git checkout -- Baldur.fwl
+git checkout -- Baldur.fwl.old
+git checkout -- script_PULL.sh
+git checkout -- script_PUSH.sh
+git pull

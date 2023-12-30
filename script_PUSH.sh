dbb9a5898cb3b68d452ab3116dcc842fb9609d39
ntumanov <grooov173@yandex.ru>
2023-12-31 01:40:26 +0400

    Обновил скрипты

--- script_PUSH.sh
+++ script_PUSH.sh
@@ -1,6 +1,11 @@
 git status
 sleep 2
-git add .
+git add Baldur.db
+git add Baldur.db.old
+git add Baldur.fwl
+git add Baldur.fwl.old
+git add script_PULL.sh
+git add script_PUSH.sh
 git commit -m "updated_state_$(date +"%d/%m/%y_%T" )"
 git push
 sleep 3

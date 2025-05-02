87ff8e290c93d019be9d75743a74d9767cbda494
Tumanov-Nikita <grooov173@yandex.ru>
2025-05-02 11:14:15 +0300

    updated_state_02/05/25_11:14:15

--- script_PULL.sh
+++ script_PULL.sh
@@ -1,15 +1,17 @@
 echo 
-read -p "Вы действительно хотите выгрузить файлы из гита (содержимое папки перезапишется)?" answer
+read -p "Вы действительно хотите выгрузить файлы из гита (содержимое папки перезапишется)? Да/Нет   " answer
 case $answer in
-y) git checkout "Village"
-git fetch
-git checkout -- Triniti.db
-git checkout -- Triniti.db.old
-git checkout -- Triniti.fwl
-git checkout -- Triniti.fwl.old
-git checkout -- script_PULL.sh
-git checkout -- script_PUSH.sh
-git pull
-n) echo "Отмена"
-sleep 2
-
+"Да" | "да") git checkout "Village";
+git fetch;
+git checkout -- Triniti.db;
+git checkout -- Triniti.db.old;
+git checkout -- Triniti.fwl;
+git checkout -- Triniti.fwl.old;
+git checkout -- script_PULL.sh;
+git checkout -- script_PUSH.sh;
+git pull;;
+"Нет" | "нет") echo "Отмена";
+sleep 1.5;;
+* )  echo "Введен некорректный ответ";
+sleep 1.5;;
+esac

c4cdee128056b4a6dceb580a0a902786656e8abf
Tumanov-Nikita <grooov173@yandex.ru>
2025-05-02 11:14:36 +0300

    updated_state_02/05/25_11:14:35

--- script_PUSH.sh
+++ script_PUSH.sh
@@ -1,9 +1,9 @@
 git status
 sleep 2
-git add Baldur.db
-git add Baldur.db.old
-git add Baldur.fwl
-git add Baldur.fwl.old
+git add Triniti.db
+git add Triniti.db.old
+git add Triniti.fwl
+git add Triniti.fwl.old
 git add script_PULL.sh
 git add script_PUSH.sh
 git commit -m "updated_state_$(date +"%d/%m/%y_%T" )"

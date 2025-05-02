fb60b1f14f3e7cd054b7ae258ec136598a532609
Tumanov-Nikita <grooov173@yandex.ru>
2025-05-02 11:02:50 +0300

    updated_state_02/05/25_11:02:50

--- script_PULL.sh
+++ script_PULL.sh
@@ -1,8 +1,15 @@
+echo 
+read -p "Вы действительно хотите выгрузить файлы из гита (содержимое папки перезапишется)?" answer
+case $answer in
+y) git checkout "Village"
 git fetch
-git checkout -- Baldur.db
-git checkout -- Baldur.db.old
-git checkout -- Baldur.fwl
-git checkout -- Baldur.fwl.old
+git checkout -- Triniti.db
+git checkout -- Triniti.db.old
+git checkout -- Triniti.fwl
+git checkout -- Triniti.fwl.old
 git checkout -- script_PULL.sh
 git checkout -- script_PUSH.sh
 git pull
+n) echo "Отмена"
+sleep 2
+
